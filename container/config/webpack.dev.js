0bd1f672db4061ee8beca056f0274a8b9e2b6f44
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2023-03-07 01:55:16 +0530

    workflow added

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common')
+const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
@@ -20,9 +19,6 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        })
     ]
 }
 
